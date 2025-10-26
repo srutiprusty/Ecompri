f98ed3e2e6106f0be0cbed79894fcb0bb9a6767e
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2025-10-27 04:31:55 +0530

    debug

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; // Import the authentication module
 // TODO: Add SDKs for Firebase products that you want to use
@@ -8,18 +8,22 @@ import { getAuth } from "firebase/auth"; // Import the authentication module
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDOh7hHPkPIry9ASM4_Y3M90qCemC10lqI",
+  authDomain: "ecompri-d81a4.firebaseapp.com",
+  projectId: "ecompri-d81a4",
+  storageBucket: "ecompri-d81a4.firebasestorage.app",
+  messagingSenderId: "1058395564291",
+  appId: "1:1058395564291:web:5034f26a61a333e8d82136",
+  measurementId: "G-HHYMCFQRQ8",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-/* const analytics = getAnalytics(app); */
-const auth = getAuth(app); // Initialize Firebase Authentication
+// Initialize Firebase only if it hasn't been initialized yet
+let app;
+let auth;
+
+if (typeof window !== "undefined" && !getApps().length) {
+  app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+}
 
 export { auth }; // Export the auth object
